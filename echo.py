115b0b4a8269d4d371bc41eb9bcb07f74aed61d2
Salvador Chinesta Llobregat <52451563+SalvaChiLlo@users.noreply.github.com>
2023-04-18 18:52:45 +0200

    Update echo.py

--- echo.py
+++ echo.py
@@ -29,7 +29,7 @@ from airflow.models import Variable
 
 with DAG(
     dag_id="example_bash_echo",
-    schedule="*/1 * * * *",
+    schedule="*/10 * * * *",
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
     dagrun_timeout=datetime.timedelta(minutes=60),
