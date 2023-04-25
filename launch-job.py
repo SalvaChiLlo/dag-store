1b780f7c9f53a9d0339e232a686217f347dc63e6
Salvador Chinesta Llobregat <52451563+SalvaChiLlo@users.noreply.github.com>
2023-04-25 13:59:59 +0200

    Update launch-job.py

--- launch-job.py
+++ launch-job.py
@@ -8,7 +8,6 @@ from airflow.decorators import dag, task
 
 log = logging.getLogger(__name__)
 
-
 if not shutil.which("virtualenv"):
     log.warning(
         "The custom_job_executor DAG requires virtualenv, please install it.")
@@ -19,24 +18,24 @@ else:
             start_date=datetime(2022, 5, 1), 
             catchup=False, 
             tags=["Custom Job Executor"],
-            params={
-                'job_dir': '/tmp/{{ dag_run.run_id }}'
-            }
+            params={}
     )
     def custom_job_executor():
 
         @task.virtualenv(
                 requirements=["gitpython==3.1.31"]
         )
-        def clone(git_url: str):
+        def clone(git_url: str, job_id: str):
             from git import Repo
             print("CLONING REPOSITORY")
 
             if git_url == None or git_url == "":
                 raise ValueError("You should provide a 'git_url'")
-            
-            Repo.clone_from(git_url, '{{ params.job_dir }}')
-            print('{{ params.job_dir }}')
+
+            if job_id == None or job_id == "":
+                raise ValueError("You should provide a 'job_id'")
+
+            Repo.clone_from(git_url, f'/tmp/{job_id}')
 
         @task()
         def install_dependencies():
@@ -55,6 +54,6 @@ else:
             print("CLEAN ENVIRONMENT")
 
 
-        clone('{{ dag_run.conf["git_url"] }}') >> install_dependencies() >> execute() >> save_results() >> clean_environment()
+        clone('{{ dag_run.conf["git_url"] }}', '{{ dag_run.conf["job_id"] }}') >> install_dependencies() >> execute() >> save_results() >> clean_environment()
 
 job_executor = custom_job_executor()
