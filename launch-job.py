63c16c1dbd9744d8a50ad66ca69bff9dd51bcea8
Salvador Chinesta Llobregat <52451563+SalvaChiLlo@users.noreply.github.com>
2023-04-25 18:03:52 +0200

    Update launch-job.py

--- launch-job.py
+++ launch-job.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import logging
 import shutil
+import subprocess
 from datetime import datetime
 
 from airflow.decorators import dag, task
@@ -14,16 +15,16 @@ if not shutil.which("virtualenv"):
 else:
 
     @dag(
-            schedule_interval=None, 
-            start_date=datetime(2022, 5, 1), 
-            catchup=False, 
-            tags=["Custom Job Executor"],
-            params={}
+        schedule_interval=None,
+        start_date=datetime(2022, 5, 1),
+        catchup=False,
+        tags=["Custom Job Executor"],
+        params={}
     )
     def custom_job_executor():
 
         @task.virtualenv(
-                requirements=["gitpython==3.1.31"]
+            requirements=["gitpython==3.1.31"]
         )
         def clone(git_url: str, job_id: str):
             from git import Repo
@@ -34,28 +35,39 @@ else:
 
             if job_id == None or job_id == "":
                 raise ValueError("You should provide a 'job_id'")
-            
+
             job_dir = f'/tmp/{job_id}'
             print(job_dir)
             Repo.clone_from(git_url, job_dir)
 
         @task()
-        def install_dependencies():
+        def install_dependencies(job_id: str):
+            job_dir = f'/tmp/{job_id}'
             print("INSTALLING DEPENDECIES")
+            with open(job_dir + '/output_dependencies.txt') as f:
+                subprocess.Popen(
+                    ['cd', job_dir, '&&', 'python install -r requirements.txt'], stdout=f, stderr=f, universal_newlines=True)
 
         @task()
-        def execute():
+        def execute(job_id: str):
             print("EXECUTE JOB")
+            job_dir = f'/tmp/{job_id}'
+            with open(job_dir + '/output_execution.txt') as f:
+                subprocess.Popen(
+                    ['cd', job_dir, '&&', 'python main.py'], stdout=f, stderr=f, universal_newlines=True)
 
         @task()
-        def save_results():
+        def save_results(job_id: str):
             print("SAVE RESULTS")
 
         @task()
-        def clean_environment():
+        def clean_environment(job_id: str):
             print("CLEAN ENVIRONMENT")
 
+        job_id = '{{ dag_run.conf["job_id"] }}'
+        git_url = '{{ dag_run.conf["git_url"] }}'
 
-        clone('{{ dag_run.conf["git_url"] }}', '{{ dag_run.conf["job_id"] }}') >> install_dependencies() >> execute() >> save_results() >> clean_environment()
+        clone(git_url, job_id) >> install_dependencies(job_id) >> execute(
+            job_id) >> save_results(job_id) >> clean_environment(job_id)
 
 job_executor = custom_job_executor()
